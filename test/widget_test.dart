835a754a6809a66d3a2ffdefaeb27edd3bb1bf13
Pablo Sotres <contact@pablosotres.com>
2021-06-16 00:19:48 -0500

    Primer commit de proyecto

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:arboles_migrantes/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const ArbolesMigrantes());
+    await tester.pumpWidget( MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
